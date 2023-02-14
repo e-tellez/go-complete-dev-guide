fd227d6e4cadcaf15179ed2848fba925c7e35f7b
e-tellez <edtllz96@gmail.com>
2023-02-13 21:11:47 -0600

    añadiendo funciones para crear el deck principal

--- 02_cards/main.go
+++ 02_cards/main.go
@@ -1,13 +1,12 @@
 package main
 
+import "fmt"
+
 func main() {
 	// Different ways of declaring variables
 	// var card string = "Ace of Spades"
 
 	cards := newDeck()
-
-	hand, remainingCards := deal(cards, 5)
-
-	hand.print()
-	remainingCards.print()
+	cards.toString()
+	fmt.Println(cards.toString())
 }
